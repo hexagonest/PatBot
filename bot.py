01ce73f30c26903064a80f76d7d925de7a09aab0
BuildTools <unconfigured@null.spigotmc.org>
2019-12-18 21:45:14 -0400

    Rewrote all the functions into much more efficient code

--- bot.py
+++ bot.py
@@ -6,224 +6,127 @@ load_dotenv()
 token=os.getenv('DISCORD_TOKEN')
 client=discord.Client(fetch_offline_members=True)
 
-#dunno what to do with this method lol, doesn't work when declared inside the class
-def randomFace():
-    faces = ["\n ヽ(~_~(・_・ )ゝ""\n⁽⁽◝( • ω • )◜⁾⁾","\n└(＾＾)┐","\n(~‾▽‾)~","\n ヽ( ⌒o⌒)人(⌒-⌒ )ﾉ","\n＼(＾▽＾)／","\n☆ ～('▽^人)","\nヽ(・∀・)ﾉ"]
-    return faces[random.randint(0,len(faces))]
-
 class PatBot:
+    def randomFace(self):
+        faces = ["\n ヽ(~_~(・_・ )ゝ""\n⁽⁽◝( • ω • )◜⁾⁾","\n└(＾＾)┐","\n(~‾▽‾)~","\n ヽ( ⌒o⌒)人(⌒-⌒ )ﾉ","\n＼(＾▽＾)／","\n☆ ～('▽^人)","\nヽ(・∀・)ﾉ"]
+        return faces[random.randint(0,len(faces))]
+
     def sendRandomError(self,message):
         return message.channel.send([" 	(⁄ ⁄•⁄ω⁄•⁄ ⁄) i don't know what to do!"," 	(⁄ ⁄•⁄ω⁄•⁄ ⁄) what is happening?","(*/ω＼) that's not right..."," 	☆ｏ(＞＜；)○ i'm not happy with the look of this... ","(ᗒᗣᗕ)՞ why is this so hard?","(＃＞＜) ... i don't know what to do... don't look at me like that!"][random.randint(0,5)])
 
-    def checkPats(self, message, user):
+    def updatePatCount(self, message, userId, amount):
+        with open("users.json") as json_file:
+            try:
+                json_members = json.load(json_file)
+            except ValueError:
+                json_members = {}
+
+        json_members[userId.id] = json_members.get(str(userId.id), 0) + amount
+
+        with open("users.json", "w") as  outfile:
+            json.dump(json_members, outfile)
+
+        return json_members[userId.id]
+
+    def sendMessage(self, message, userId, content):
+        return message.channel.send(content)
+
+    def logMessage(self, content):
         time = datetime.now().strftime("[%D %H:%M:%S]")
-        if user == message.author:
-            print('|'+ time +'| >{0.author} just checked his pat points.'.format(message))
-            with open("users.json") as json_file:
-                try:
-                    json_members = json.load(json_file)
-                except ValueError:
-                    json_members = {}
-                if(json_members.get(str(message.author.id), 0) != 1):
-                    return message.channel.send("{0.author.mention}, you have {1} pat points! {2}".format(message, json_members.get(str(message.author.id), 0), randomFace()))
-                else:
-                    return message.channel.send("{0.author.mention}, you have {1} pat point! {2}".format(message, json_members.get(str(message.author.id), 0), randomFace()))
+        return print('|{0}| : {1}'.format(time, content))
+
+    def checkPats(self, message, userId):
+        patCount = self.updatePatCount(message, userId, 0)
+        if userId == message.author:
+            self.logMessage('{0.author} just checked his pat points.'.format(message))
+            if patCount != 1:
+                return self.sendMessage(message, userId, "{0.author.mention}, you have {1} pat points! {2}".format(message, patCount, self.randomFace()))
+            else:
+                return self.sendMessage(message, userId, "{0.author.mention}, you have {1} pat point! {2}".format(message, patCount, self.randomFace()))
         else:
-            #print(user)
-            print("|"+ time +"| >{0.author} just checked {1}'s pat points.".format(message, user))
-            with open("users.json") as json_file:
-                try:
-                    json_members = json.load(json_file)
-                except ValueError:
-                    json_members = {}
-                if(json_members.get(str(message.author.id), 0) != 1):
-                    return message.channel.send("{0.mention} has {1} pat points! {2}".format(user, json_members.get(str(user.id), 0), randomFace()))
-                else:
-                    return message.channel.send("{0.mention} has {1} pat point! {2}".format(user, json_members.get(str(user.id), 0), randomFace()))
+            self.logMessage("{0.author} just checked {1}'s pat points.".format(message, userId))
+            if patCount != 1:
+                return self.sendMessage(message, userId, "{0.mention} has {1} pat points! {2}".format(userId, patCount, self.randomFace()))
+            else:
+                return self.sendMessage(message, userId, "{0.mention} has {1} pat point! {2}".format(userId, patCount, self.randomFace()))
+
 
     def pat(self, message, userId):
-        time = datetime.now().strftime("[%D %H:%M:%S]")
-        try:
-            patee = userId
-        except:
-            return
+        self.updatePatCount(message, userId, 1) #1 means +1 on the patcount of the target user.
 
-        with open("users.json") as json_file:
-            try:
-                json_members = json.load(json_file)
-            except ValueError:
-                json_members = {}
-
-        json_members[userId.id] = json_members.get(str(userId.id), 0) + 1
-
-
-        with open("users.json", "w") as  outfile:
-            json.dump(json_members, outfile)
-
-        if patee == message.author:
-            print('|'+ time +'| >{0.author} patted themselves.'.format(message))
-            return message.channel.send('[+1pp] {0.author.mention} patted themselves... \n(￢_￢;) (つд｀)'.format(message))
+        if userId == message.author: #check if the user is the target of the command
+            self.logMessage('{0.author} patted themselves.'.format(message))
+            return self.sendMessage(message, userId, '[+1pp] {0.author.mention} patted themselves... \n(￢_￢;) (つд｀)'.format(message))
         else:
-            print('|'+ time +'| >{0.author} just patted {1}.'.format(message, patee))
-            return message.channel.send('[+1pp] {0.author.mention} pats {1.mention}! \n(≧ω≦)ԅ( ˘⌣˘)'.format(message, patee))
+            self.logMessage('{0.author} just patted {1}.'.format(message, userId))
+            return self.sendMessage(message, userId, '[+1pp] {0.author.mention} pats {1.mention}! \n(≧ω≦)ԅ( ˘⌣˘)'.format(message, userId))
+
 
     def slap(self, message, userId):
-        time = datetime.now().strftime("[%D %H:%M:%S]")
-        try:
-            patee = userId
-        except:
-            return
+        self.updatePatCount(message, userId, -1) #1 means +1 on the patcount of the target user.
 
-        with open("users.json") as json_file:
-            try:
-                json_members = json.load(json_file)
-            except ValueError:
-                json_members = {}
-
-        json_members[userId.id] = json_members.get(str(userId.id), 0) - 1
-
-        with open("users.json", "w") as  outfile:
-            json.dump(json_members, outfile)
-
-        if patee == message.author:
-            ##debug cmd line
-            print('|'+ time +'| >{0.author} just slapped themselves.'.format(message))
-            return message.channel.send('[-1 pp] {0.author.mention} just slapped themselves... \n(つ✖╭╮✖)   	╮(￣ω￣;)╭'.format(message))
+        if userId == message.author: #check if the user is the target of the command
+            self.logMessage('{0.author} just slapped themselves.'.format(message))
+            return self.sendMessage(message, userId, '[-1 pp] {0.author.mention} just slapped themselves... \n(つ✖╭╮✖)   	╮(￣ω￣;)╭'.format(message))
         else:
-            ##debug
-            print('|'+ time +'| >{0.author} just slapped {1}.'.format(message, patee))
-            return message.channel.send('[-1 pp] {0.author.mention} slaps {1.mention}! \nᕦ(ò_óˇ)ᕤ    Σ(°་།°)'.format(message, patee))
+            self.logMessage('{0.author} just slapped {1}.'.format(message, userId))
+            return self.sendMessage(message, userId, '[-1 pp] {0.author.mention} slaps {1.mention}! \nᕦ(ò_óˇ)ᕤ    Σ(°་།°)'.format(message, userId))
+        return
 
     def kick(self, message, userId):
-        time = datetime.now().strftime("[%D %H:%M:%S]")
-        try:
-            patee = userId
-        except:
-            return
+        self.updatePatCount(message, userId, -3) #1 means +1 on the patcount of the target user.
 
-        with open("users.json") as json_file:
-            try:
-                json_members = json.load(json_file)
-            except ValueError:
-                json_members = {}
-
-        json_members[userId.id] = json_members.get(str(userId.id), 0) - 3
-
-        with open("users.json", "w") as  outfile:
-            json.dump(json_members, outfile)
-
-
-        if patee == message.author:
-            ##debug cmd line
-            print('|'+ time +'| >{0.author} just kicked themselves.'.format(message))
-            return message.channel.send('[-3 pp] {0.author.mention} just kicked themselves... \n_:(´ཀ`」 ∠):_     ლ(ಠ_ಠლ) '.format(message))
+        if userId == message.author: #check if the user is the target of the command
+            self.logMessage('{0.author} just kicked themselves.'.format(message))
+            return self.sendMessage(message, userId, '[-3 pp] {0.author.mention} just kicked themselves... \n_:(´ཀ`」 ∠):_     ლ(ಠ_ಠლ) '.format(message))
         else:
-            ##debug
-            print('|'+ time +'| >{0.author} just kicked {1}. Ouch'.format(message, patee))
-            return message.channel.send('[-3 pp] {0.author.mention} kicks {1.mention}! \n(ノಠ益ಠ)ノ彡＼＼٩(๑`^´๑)۶／／'.format(message, patee))
+            self.logMessage('{0.author} just kicked {1}.'.format(message, userId))
+            return self.sendMessage(message, userId, '[-3 pp] {0.author.mention} kicks {1.mention}! \n(ノಠ益ಠ)ノ彡＼＼٩(๑`^´๑)۶／／'.format(message, userId))
+        return
 
     def hug(self, message, userId):
-        time = datetime.now().strftime("[%D %H:%M:%S]")
-        try:
-            patee = userId
-        except:
-            return
+        self.updatePatCount(message, userId, 3) #1 means +1 on the patcount of the target user.
 
-        with open("users.json") as json_file:
-            try:
-                json_members = json.load(json_file)
-            except ValueError:
-                json_members = {}
-
-        json_members[userId.id] = json_members.get(str(userId.id), 0) + 3
-
-        with open("users.json", "w") as  outfile:
-            json.dump(json_members, outfile)
-
-        if patee == message.author:
-            ##debug cmd line
-            print('|'+ time +'| >{0.author} just hugged themselves.'.format(message))
-            return message.channel.send('[+3pp] {0.author.mention} just hugged themselves...  \n(つ . •́ _ʖ •̀ .)つ'.format(message))
+        if userId == message.author: #check if the user is the target of the command
+            self.logMessage('{0.author} just hugged themselves.'.format(message))
+            return self.sendMessage(message, userId, '[+3pp] {0.author.mention} just hugged themselves...  \n(つ . •́ _ʖ •̀ .)つ'.format(message))
         else:
-            ##debug
-            print('|'+ time +'| >{0.author} just hugged {1}.'.format(message, patee))
-            return message.channel.send('[+3pp] {0.author.mention} hugs {1.mention}! \n (づ^-^(^ ^*)つ ♡'.format(message, patee))
-
+            self.logMessage('{0.author} just hugged {1}.'.format(message, userId))
+            return self.sendMessage(message, userId, '[+3pp] {0.author.mention} hugs {1.mention}! \n (づ^-^(^ ^*)つ ♡'.format(message, userId))
+        return
 
     def sesh(self, message, userId):
-        time = datetime.now().strftime("[%D %H:%M:%S]")
-        try:
-            patee = userId
-        except:
-            return
+        self.updatePatCount(message, userId, 10) #1 means +1 on the patcount of the target user.
 
-        with open("users.json") as json_file:
-            try:
-                json_members = json.load(json_file)
-            except ValueError:
-                json_members = {}
-
-        json_members[userId.id] = json_members.get(str(userId.id), 0) + 10
-
-        with open("users.json", "w") as  outfile:
-            json.dump(json_members, outfile)
-
-        if patee.mention == message.author.mention:
-            ##debug cmd line
-            print('|'+ time +'| >{0.author} just seshed alone.'.format(message))
-            return message.channel.send('[+10pp] {0.author.mention} just seshed alone. \n ౦０o ｡ (‾́。‾́  )y~~'.format(message))
+        if userId == message.author: #check if the user is the target of the command
+            self.logMessage('{0.author} just seshed by themselves.'.format(message))
+            return self.sendMessage(message, userId, '[+10pp] {0.author.mention} just seshed alone. \n ౦０o ｡ (‾́。‾́  )y~~'.format(message))
         else:
-            ##debug
-            print('|'+ time +'| >{0.author} just hugged {1}.'.format(message, patee))
-            return message.channel.send('[+10pp] {0.author.mention} seshes with {1.mention}! \n ( ≖ ͜ʖ≖)౦０౦０o ｡(°ε° )y~~'.format(message, patee))
+            self.logMessage('{0.author} just seshed with {1}.'.format(message, userId))
+            return self.sendMessage(message, userId, '[+10pp] {0.author.mention} seshes with {1.mention}! \n ( ≖ ͜ʖ≖)౦０౦０o ｡(°ε° )y~~'.format(message, userId))
+        return
 
     def mlem(self, message, userId):
-        time = datetime.now().strftime("[%D %H:%M:%S]")
-        try:
-            patee = userId
-        except:
-            return
+        self.updatePatCount(message, userId, 15) #1 means +1 on the patcount of the target user.
 
-        with open("users.json") as json_file:
-            try:
-                json_members = json.load(json_file)
-            except ValueError:
-                json_members = {}
-
-        json_members[userId.id] = json_members.get(str(userId.id), 0) + 1
-
-        with open("users.json", "w") as  outfile:
-            json.dump(json_members, outfile)
-
-        if patee.mention == message.author.mention:
-            ##debug cmd line
-            print('|'+ time +'| >{0.author} just mlemed.'.format(message))
-            return message.channel.send('[+1pp] {0.author.mention} mlems\n (≧ڡ≦*)'.format(message))
+        if userId == message.author: #check if the user is the target of the command
+            self.logMessage('{0.author} just mlemed'.format(message))
+            return self.sendMessage(message, userId, '[+15pp] {0.author.mention} mlems\n (≧ڡ≦*)'.format(message))
         else:
-            ##debug
-            print('|'+ time +'| >{0.author} just mlemed {1}.'.format(message, patee))
-            return message.channel.send('[+1pp] {0.author.mention} mlems at {1.mention}!\n  (ˆڡˆ)｡⌒☆(´ ω `)'.format(message, patee))
+            self.logMessage('{0.author} just mlemed at {1}.'.format(message, userId))
+            return self.sendMessage(message, userId, '[+15pp] {0.author.mention} mlems at {1.mention}!\n  (ˆڡˆ)｡⌒☆(´ ω `)'.format(message, userId))
+        return
 
     def muang(self, message, userId):
-        time = datetime.now().strftime("[%D %H:%M:%S]")
-        try:
-            patee = userId
-        except:
-            return
+        if userId == message.author:
+            self.logMessage('{0.author} muang'.format(message))
+            return self.sendMessage(message, userId, 'muang! \n (^=◕ᴥ◕=^) V●ᴥ●V muang!')
 
-        if patee.mention == message.author.mention:
-            ##debug cmd line
-            print('|'+ time +'| >{0.author} muang'.format(message))
-            return message.channel.send('muang! \n (^=◕ᴥ◕=^) V●ᴥ●V muang!'.format(message))
-
-    #TODO convert to commaands
+patbot = PatBot()
 
 @client.event
 async def on_ready():
-    time = datetime.now().strftime("[%D %H:%M:%S]")
-    print('|'+ time +'|-[hexadecimalbot] patBot online.')
-
-patbot = PatBot()
+    patbot.logMessage("patbot online ...")
 
 @client.event
 async def on_message(message):
@@ -235,17 +138,15 @@ async def on_message(message):
 
     args = message.content.split(' ')
     userId = message.mentions[0] if 0 < len(message.mentions) else message.author
-
-
     #if an argument is present, assign target to user. If not, assign target to sender of message.
 
     if message.content.startswith('%pats'):
-        try:
+        #try:
             await patbot.checkPats(message,userId)
             return
-        except:
-            await patbot.sendRandomError(message)
-            return
+        #except:
+        #    await patbot.sendRandomError(message)
+        #    return
     elif message.content.startswith('%pat'):
         try:
             await patbot.pat(message, userId)
